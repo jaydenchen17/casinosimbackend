b0bb66b148f4167fdad91475c2d5741f01fa031e
brandonso36 <bbrandons0509@gmail.com>
2024-01-24 10:33:31 -0800

    clean up

--- api/money.py
+++ api/money.py
@@ -12,33 +12,16 @@ CORS(money_api, resources={r"/api/money/create": {"origins": "http://127.0.0.1:8
 
 chat_data = []
 
-class ChatAPI:
+class MoneyAPI:
     class _Test(Resource):
         def get(self):
             response = jsonify({"Connection Test": "Successfully connected to backend!"})
             return response
        
-api.add_resource(ChatAPI._Test, '/test')
+api.add_resource(MoneyAPI._Test, '/test')
 
 if __name__ == "__main__":
     server = "http://127.0.0.1:8987" # run local
     # server = 'https://chat.stu.nighthawkcodingsociety.com'  # Update with your server URL
     url = server + "/api/money"
     responses = []
-
-    # Simulate sending data to the chat API
-    sample_data = {"message": "Hello, this is a test message!"}
-    create_response = requests.post(url+"/create", json=sample_data)
-    responses.append(create_response)
-
-    # Retrieve stored data from the chat API
-    read_response = requests.get(url+"/read")
-    responses.append(read_response)
-
-    # Display responses
-    for response in responses:
-        print(response)
-        try:
-            print(response.json())
-        except:
-            print("unknown error")
